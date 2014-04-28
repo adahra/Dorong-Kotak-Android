21b5d5da04b8ce7778f7e837f23318a85a9c2908
nauin <adnantoa@gmail.com>
2014-04-28 12:03:37 +0700

    Class hasil auto compile

--- gen/org/gete/android/dorongkotak/R.java
+++ gen/org/gete/android/dorongkotak/R.java
@@ -984,19 +984,15 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int back=0x7f020057;
-        public static final int crate=0x7f020058;
-        public static final int credits=0x7f020059;
+        public static final int credits=0x7f020058;
+        public static final int empty=0x7f020059;
         public static final int exit=0x7f02005a;
-        public static final int floor=0x7f02005b;
-        public static final int goal=0x7f02005c;
-        public static final int gothicforest=0x7f02005d;
-        public static final int ic_launcher=0x7f02005e;
-        public static final int options=0x7f02005f;
-        public static final int playgame=0x7f020060;
-        public static final int sokoban=0x7f020061;
-        public static final int splash=0x7f020062;
-        public static final int titlepict=0x7f020063;
-        public static final int wall=0x7f020064;
+        public static final int gothicforest=0x7f02005b;
+        public static final int ic_launcher=0x7f02005c;
+        public static final int options=0x7f02005d;
+        public static final int playgame=0x7f02005e;
+        public static final int splash=0x7f02005f;
+        public static final int titlepict=0x7f020060;
     }
     public static final class id {
         public static final int action_bar=0x7f06001c;
