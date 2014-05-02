0085cd4b9210315e3c380c77fde04c57a530db5b
nauin <adnantoa@gmail.com>
2014-05-02 14:34:56 +0700

    Perubahan pada berkas

--- gen/org/gete/android/dorongkotak/R.java
+++ gen/org/gete/android/dorongkotak/R.java
@@ -1130,7 +1130,8 @@ containing a value of this type.
     }
     public static final class raw {
         public static final int gamemap=0x7f050000;
-        public static final int menuscreenmusic=0x7f050001;
+        public static final int image=0x7f050001;
+        public static final int menuscreenmusic=0x7f050002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
