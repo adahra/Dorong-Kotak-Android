d253f8e6f3af1f3e416e71a379ca6c35bc61baf7
nauin <adnantoa@gmail.com>
2014-05-02 14:31:05 +0700

    Kelas yang digunakan untuk menangani asset dalam game berupa suara dan gambar

---
+++ src/org/gete/android/dorongkotak/Asset.java
@@ -0,0 +1,20 @@
+package org.gete.android.dorongkotak;
+
+import org.gete.android.framework.Image;
+
+public class Asset {
+	public static Image actorBack;
+	public static Image actorFront;
+	public static Image actorLeft;
+	public static Image actorRight;
+	public static Image crate;
+	public static Image floor;
+	public static Image wall;
+	public static Image wallCornerLeft;
+	public static Image wallCornerRight;
+	public static Image wallLeftFloor;
+	public static Image wallRightFloor;
+	public static Image woodenHole;
+	public static Image splash;
+	
+}
