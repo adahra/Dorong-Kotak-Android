1bf1f7c5cc740abd8620d5e8cfeafddb07f978db
nauin <adnantoa@gmail.com>
2014-05-08 12:59:19 +0700

    Kelas yang digunakan untuk menampilkan arena game

--- src/org/gete/android/dorongkotak/GameView.java
+++ src/org/gete/android/dorongkotak/GameView.java
@@ -2,17 +2,31 @@ package org.gete.android.dorongkotak;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewTreeObserver.OnTouchModeChangeListener;
 
-public class GameView extends Activity {
+public class GameView extends Activity implements OnTouchListener, OnTouchModeChangeListener {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(new DrawingPanel(this));
+		setContentView(R.layout.game_view);
 	}
 	
 	@Override
 	public void onBackPressed() {
 		GameView.this.finish();
+	}
+
+	@Override
+	public void onTouchModeChanged(boolean isInTouchMode) {
+		
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return false;
 	}
 }
