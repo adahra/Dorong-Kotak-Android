94b1e60ec1bc380099ead6798aa315c16f9f09ff
nauin <adnantoa@gmail.com>
2014-05-08 13:03:30 +0700

    Penghapusan berkas game framework

--- src/org/gete/android/framework/Audio.java
+++
@@ -1,8 +0,0 @@
-package org.gete.android.framework;
-
-public interface Audio {
-	
-	public Music createMusic(String fileName);
-	
-	public Sound createSound(String fileName);
-}
--- src/org/gete/android/framework/Sound.java
+++
@@ -1,8 +0,0 @@
-package org.gete.android.framework;
-
-public interface Sound {
-	
-	public void play(float volume);
-	
-	public void dispose();
-}
