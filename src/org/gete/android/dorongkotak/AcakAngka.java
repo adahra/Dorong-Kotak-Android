60cdfbe73f2c56f76ae33815eaa5833787b7f6fe
nauin <adnantoa@gmail.com>
2014-05-22 13:14:56 +0700

    Kelas yang digunakan untuk membuat angka acak

---
+++ src/org/gete/android/dorongkotak/AcakAngka.java
@@ -0,0 +1,11 @@
+package org.gete.android.dorongkotak;
+
+/**
+ * Kelas yang digunakan untuk mengacak nomor/angka yang akan
+ * ditampilkan dalam game yang berguna untuk dijawab pemain
+ * @author Adnanto Ahmad Ramadhon
+ *
+ */
+public class AcakAngka {
+
+}
