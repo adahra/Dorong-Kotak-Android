908b5ff2c668b4e571ff32a343a29777e0a235c9
nauin <adnantoa@gmail.com>
2014-06-18 23:45:35 +0700

    Perubahan pada berkas MenuView.java

--- src/org/gete/android/dorongkotak/MenuView.java
+++ src/org/gete/android/dorongkotak/MenuView.java
@@ -1,77 +1,76 @@
 package org.gete.android.dorongkotak;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Process;
 import android.util.Log;
 import android.view.View;
+import org.gete.android.dorongkotak.R;
 
 /**
  * Kelas yang digunakan untuk mengatur tampilan dari menu
  * @author Adnanto Ahmad Ramadhon
  *
  */
-public class MenuView extends Activity {
-	private MediaPlayer mBackgroundMusic;
+public class MenuView extends Activity {	
 	private Intent mIntent;
 	private static String TAG = "MenuView";
+	private Context context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu_view);
-		mBackgroundMusic = MediaPlayer.create(this, R.raw.menuscreenmusic);
-		mBackgroundMusic.setLooping(true);
-		mBackgroundMusic.setVolume(1, 1);
-		mBackgroundMusic.start();
+		context = getApplicationContext();
+		PemutarSuara.muatSuara(context);
+		PemutarSuara.mainkanMusikSatu();
 		Log.d(TAG, "onCreate(savedInstanceState");
 	}
 	
 	public void tampilGame(View view) {
 		mIntent = new Intent(MenuView.this, Play.class);
-		startActivity(mIntent);
+		MenuView.this.startActivity(mIntent);
 	}
 	
 	public void tampilKredit(View view) {
 		mIntent = new Intent(MenuView.this, CreditView.class);
-		startActivity(mIntent);
+		MenuView.this.startActivity(mIntent);
 	}
 	
 	public void keluar(View view) {
-		mBackgroundMusic.stop();
-		mBackgroundMusic.release();
-		hentikanProses();
+		keluar();
 	}
 	
 	@Override
 	public void onDestroy() {
+		PemutarSuara.hentikanMusikSatu();
+		keluar();
 		super.onDestroy();
-		mBackgroundMusic.release();
-		hentikanProses();
 	}
 	
 	@Override
 	public void onResume() {
-		mBackgroundMusic.start();
 		super.onResume();
+		PemutarSuara.mainkanMusikSatu();
 	}
 	
 	@Override
 	public void onPause() {
-		mBackgroundMusic.pause();
+		PemutarSuara.pauseMusikSatu();
 		super.onPause();
 	}
 	
 	@Override
 	public void onBackPressed() {
-		mBackgroundMusic.stop();
-		hentikanProses();
+    	super.onBackPressed();
+    	keluar();
 	}
 	
-	public void hentikanProses() {
-		Process.killProcess(Process.myPid());
+	private void keluar() {
+		PemutarSuara.hentikanMusikSatu();
+    	Process.killProcess(Process.myPid());
 	}
 	
 }
