36e66cc6719d999dda27394374b55519be295869
nauin <adnantoa@gmail.com>
2014-06-18 23:46:22 +0700

    Perubahan pada berkas CreditView.java

--- src/org/gete/android/dorongkotak/CreditView.java
+++ src/org/gete/android/dorongkotak/CreditView.java
@@ -1,8 +1,12 @@
 package org.gete.android.dorongkotak;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import org.gete.android.dorongkotak.R;
 
 /**
  * Kelas yang digunakan untuk menampilkan layar tentang aplikasi
@@ -10,15 +14,31 @@ import android.view.View;
  *
  */
 public class CreditView extends Activity {
+	private Context mContext;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_credit_view);
+		mContext = getApplicationContext();
+		PemutarSuara.muatSuara(mContext);
+		PemutarSuara.mainkanMusikSatu();
 	}
 	
 	public void kembali(View view) {
-		CreditView.this.finish();
+		kembali();
 	}
-		
+	
+	@Override
+	public void onBackPressed() {
+    	super.onBackPressed();
+    	kembali();
+	}
+	
+	private void kembali() {
+		PemutarSuara.hentikanMusikSatu();
+    	Intent intent = new Intent(CreditView.this, MenuView.class);
+    	CreditView.this.startActivity(intent);
+    	CreditView.this.finish();
+	}
 }
